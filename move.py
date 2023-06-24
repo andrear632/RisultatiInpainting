59955ee2e9223c723c0edc330762bd7f3df556c3
Andrea <andrear632@gmail.com>
2023-06-24 12:23:30 +0200

    .

--- move.py
+++ move.py
@@ -1,9 +1,9 @@
 import shutil
 
 for i in range(1, 9):
-    path_from = '/Users/andrearodriguez/Downloads/output/'+str(i)+'_output_0.wav'
-    #path_from = '/Users/andrearodriguez/Downloads/output/'+str(i)+'_inpainted.wav'
+    #path_from = '/Users/andrearodriguez/Downloads/output/'+str(i)+'_output_0.wav'
+    path_from = '/Users/andrearodriguez/Downloads/output/'+str(i)+'_inpainted.wav'
 
-    path_to = '/Users/andrearodriguez/rep/RisultatiInpainting/clip'+str(i)+'/audioldm_2.wav'
+    path_to = '/Users/andrearodriguez/rep/RisultatiInpainting/clip'+str(i)+'/ddnm_2.wav'
 
     shutil.move(path_from, path_to)
