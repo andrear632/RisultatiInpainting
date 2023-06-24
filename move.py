36ab56462132adc5607226f1cd0382a41192ae4e
Andrea <andrear632@gmail.com>
2023-06-24 13:12:29 +0200

    final? updated clips

--- move.py
+++ move.py
@@ -2,8 +2,8 @@ import shutil
 
 for i in range(1, 9):
     #path_from = '/Users/andrearodriguez/Downloads/output/'+str(i)+'_output_0.wav'
-    path_from = '/Users/andrearodriguez/Downloads/output/'+str(i)+'_inpainted.wav'
+    path_from = '/Users/andrearodriguez/Downloads/output/'+str(i)+'_original.wav'
 
-    path_to = '/Users/andrearodriguez/rep/RisultatiInpainting/clip'+str(i)+'/ddnm_2.wav'
+    path_to = '/Users/andrearodriguez/rep/RisultatiInpainting/clip'+str(i)+'/original.wav'
 
     shutil.move(path_from, path_to)
