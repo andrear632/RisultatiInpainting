4dabed0966bd822d6c962b2d2d33dce95190ba25
Andrea <andrear632@gmail.com>
2023-06-28 23:15:33 +0200

    denoising

--- crop.py
+++ crop.py
@@ -1,20 +1,21 @@
 from PIL import Image
 
-for i in range(1, 9):
-    im = Image.open('/Users/andrearodriguez/Downloads/output/'+str(i)+'_mel-spectrograms.png')
+for i in range(1, 5):
+    im = Image.open('/Users/andrearodriguez/Downloads/denoising/output_ddnm_20_2s/'+str(i)+'_mel-spectrograms.png')
 
+    # third
     left = 756
     top = 32
     right = 1088
     bottom = 293
 
-    # original
+    # first
     #left = 98
     #top = 32
     #right = 430
     #bottom = 293
 
-    # cut
+    # second
     #left = 427
     #top = 32
     #right = 759
@@ -23,18 +24,4 @@ for i in range(1, 9):
     im1 = im.crop((left, top, right, bottom))
     
     #im1.show()
-    #im1.save("/Users/andrearodriguez/rep/RisultatiInpainting/clip"+str(i)+"/original.png")
-    #im1.save("/Users/andrearodriguez/rep/RisultatiInpainting/clip"+str(i)+"/cut_1.png")
-    #im1.save("/Users/andrearodriguez/rep/RisultatiInpainting/clip"+str(i)+"/cut_2.png")
-    im1.save("/Users/andrearodriguez/rep/RisultatiInpainting/clip"+str(i)+"/audioldm_1.png")
-    #im1.save("/Users/andrearodriguez/rep/RisultatiInpainting/clip"+str(i)+"/audioldm_2.png")
-    #im1.save("/Users/andrearodriguez/rep/RisultatiInpainting/clip"+str(i)+"/tango_1.png")
-    #im1.save("/Users/andrearodriguez/rep/RisultatiInpainting/clip"+str(i)+"/tango_2.png")
-    #im1.save("/Users/andrearodriguez/rep/RisultatiInpainting/clip"+str(i)+"/ddnm_1.png")
-    #im1.save("/Users/andrearodriguez/rep/RisultatiInpainting/clip"+str(i)+"/ddnm_2.png")
-    #im1.save("/Users/andrearodriguez/rep/RisultatiInpainting/clip"+str(i)+"/ddnm+_1.png")
-    #im1.save("/Users/andrearodriguez/rep/RisultatiInpainting/clip"+str(i)+"/ddnm+_2.png")
-    #im1.save("/Users/andrearodriguez/rep/RisultatiInpainting/clip"+str(i)+"/repaint_1.png")
-    #im1.save("/Users/andrearodriguez/rep/RisultatiInpainting/clip"+str(i)+"/repaint_2.png")
-    #im1.save("/Users/andrearodriguez/rep/RisultatiInpainting/clip"+str(i)+"/repaintjump_1.png")
-    #im1.save("/Users/andrearodriguez/rep/RisultatiInpainting/clip"+str(i)+"/repaintjump_2.png")
+    im1.save("/Users/andrearodriguez/rep/RisultatiInpainting/denoising/clip"+str(i)+"/clean_20_gap_2.png")
