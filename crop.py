dd045f1d03081cb8438bca98a0acf3215f921f69
Andrea <andrear632@gmail.com>
2023-06-29 15:49:46 +0200

    new noisy results

--- crop.py
+++ crop.py
@@ -1,13 +1,13 @@
 from PIL import Image
 
 for i in range(1, 5):
-    im = Image.open('/Users/andrearodriguez/Downloads/denoising/output_ddnm_20_2s/'+str(i)+'_mel-spectrograms.png')
+    im = Image.open('/Users/andrearodriguez/Downloads/output_ddnm_20_1s/'+str(i)+'_mel-spectrograms.png')
 
     # third
-    left = 756
-    top = 32
-    right = 1088
-    bottom = 293
+    #left = 756
+    #top = 32
+    #right = 1088
+    #bottom = 293
 
     # first
     #left = 98
@@ -16,12 +16,12 @@ for i in range(1, 5):
     #bottom = 293
 
     # second
-    #left = 427
-    #top = 32
-    #right = 759
-    #bottom = 293
+    left = 427
+    top = 32
+    right = 759
+    bottom = 293
     
     im1 = im.crop((left, top, right, bottom))
     
     #im1.show()
-    im1.save("/Users/andrearodriguez/rep/RisultatiInpainting/denoising/clip"+str(i)+"/clean_20_gap_2.png")
+    im1.save("/Users/andrearodriguez/rep/RisultatiInpainting/denoising/clip"+str(i)+"/noisy_20_gap_1.png")
